c375cf67619201ef63928e359bb283cbb1582c2e
gustahogt <gustahogt@gmail.com>
2026-09-16 20:45:58 +0000

    EjerciciosSemana2

---
+++ EjecutarBanco.java
@@ -0,0 +1,8 @@
+public class EjecutarBanco {
+    public static void main(String[] args) {
+
+        Cuenta objCuenta1 = new Cuenta(1010, "Juan", "587-3", "Ahorros", 0.0);
+        System.out.println(objCuenta1.toString());
+
+    }
+}
